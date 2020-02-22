1fdfd4f87b64ed60d77e79ccd6dc0eeed8eb2dac
LuisFelipeP98 <61294912+LuisFelipeP98@users.noreply.github.com>
2020-02-22 15:26:59 -0600

    insert procedure curso

--- BD/gaBD_procedimientos.sql
+++ BD/gaBD_procedimientos.sql
@@ -4,10 +4,20 @@
 -- drop procedure ObtieneTODOS_CURSOS;
 
 DELIMITER //
-CREATE PROCEDURE ObtieneTODOS_CURSOS()
+CREATE PROCEDURE PRC_ObtieneTODOS_CURSOS()
 BEGIN
 	SELECT codigo, carrera_codigo, anio, ciclo, nombre,creditos,horas_semanales FROM gadb.curso;
 END 
+//
+DELIMITER ;
+
+-- drop procedure PRC_INS_CURSO;
+DELIMITER //
+CREATE PROCEDURE PRC_INS_CURSO(codigo_ varchar(10), carrera_codigo_ varchar(10),anio_ varchar(15),ciclo_ varchar(15),nombre_ varchar(100), creditos_ int, horas_semanales_ int)
+BEGIN
+	insert into gabd.curso (codigo,carrera_codigo,anio,ciclo,nombre,creditos,horas_semanales) 
+	values (codigo_,carrera_codigo_,anio_,ciclo_,nombre_,nombre_,creditos_,horas_semanales_);
+END
 //
 DELIMITER ;
 
