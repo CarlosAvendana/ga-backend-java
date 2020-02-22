3b84f4f99e98591882bdb680e5421e4ba537fff7
LuisFelipeP98 <61294912+LuisFelipeP98@users.noreply.github.com>
2020-02-22 15:52:24 -0600

    procedure cursos

--- BD/gaBD_procedimientos.sql
+++ BD/gaBD_procedimientos.sql
@@ -1,7 +1,6 @@
 -- Procedimientos para cursos
 
-
--- drop procedure ObtieneTODOS_CURSOS;
+drop procedure IF EXISTS PRC_ObtieneTODOS_CURSOS;
 
 DELIMITER //
 CREATE PROCEDURE PRC_ObtieneTODOS_CURSOS()
@@ -11,13 +10,38 @@ END
 //
 DELIMITER ;
 
--- drop procedure PRC_INS_CURSO;
+drop procedure IF EXIST PRC_INS_CURSO;
+
 DELIMITER //
 CREATE PROCEDURE PRC_INS_CURSO(codigo_ varchar(10), carrera_codigo_ varchar(10),anio_ varchar(15),ciclo_ varchar(15),nombre_ varchar(100), creditos_ int, horas_semanales_ int)
 BEGIN
 	insert into gabd.curso (codigo,carrera_codigo,anio,ciclo,nombre,creditos,horas_semanales) 
-	values (codigo_,carrera_codigo_,anio_,ciclo_,nombre_,nombre_,creditos_,horas_semanales_);
+	values (codigo_,carrera_codigo_,anio_,ciclo_,nombre_,creditos_,horas_semanales_);
 END
 //
 DELIMITER ;
+
+drop procedure IF EXIST PRC_UPD_CURSO;
+
+DELIMITER //
+CREATE PROCEDURE PRC_UPD_CURSO(codigo_ varchar(10), carrera_codigo_ varchar(10),anio_ varchar(15),ciclo_ varchar(15),nombre_ varchar(100), creditos_ int, horas_semanales_ int)
+BEGIN
+	update gabd.curso
+	set anio=anio_,ciclo=ciclo_,nombre=nombre_,creditos=creditos_,horas_semanales=horas_semanales
+	where codigo=codigo_ and carrera_codigo=carrera_codigo_;
+END
+//
+DELIMITER ; 
+
+drop procedure if exist PRC_DEL_CURSO;
+
+DELIMITER //
+CREATE PROCEDURE PRC_DEL_CURSO(codigo_ varchar(10))
+BEGIN 
+	delete from gabd.curso 
+	where codigo=codigo_;
+END
+//
+DELIMITER ;
+
 
