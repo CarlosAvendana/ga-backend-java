890322091e25cfdeadbfa378db29d10fb14472c5
LuisFelipeP98 <61294912+LuisFelipeP98@users.noreply.github.com>
2020-02-22 17:00:56 -0600

    correcion

--- BD/gaBD_procedimientos.sql
+++ BD/gaBD_procedimientos.sql
@@ -1,11 +1,11 @@
-use gabd; 
+use gadb; 
 
 DROP PROCEDURE IF EXISTS PRC_INS_CURSO;
 
 DELIMITER //
 CREATE PROCEDURE PRC_INS_CURSO(codigo_ varchar(10), carrera_codigo_ varchar(10),anio_ varchar(15),ciclo_ varchar(15),nombre_ varchar(100), creditos_ int, horas_semanales_ int)
 BEGIN
-	insert into gabd.curso (codigo,carrera_codigo,anio,ciclo,nombre,creditos,horas_semanales) 
+	insert into gadb.curso (codigo,carrera_codigo,anio,ciclo,nombre,creditos,horas_semanales) 
 	values (codigo_,carrera_codigo_,anio_,ciclo_,nombre_,creditos_,horas_semanales_);
 END
 //
@@ -16,7 +16,7 @@ DROP PROCEDURE IF EXISTS PRC_DEL_CURSO;
 DELIMITER //
 CREATE PROCEDURE PRC_DEL_CURSO(codigo_ varchar(10))
 BEGIN 
-	delete from gabd.curso 
+	delete from gadb.curso 
 	where codigo=codigo_;
 END
 //
@@ -27,7 +27,7 @@ DROP PROCEDURE IF EXISTS PRC_UPD_CURSO;
 DELIMITER //
 CREATE PROCEDURE PRC_UPD_CURSO(codigo_ varchar(10), carrera_codigo_ varchar(10),anio_ varchar(15),ciclo_ varchar(15),nombre_ varchar(100), creditos_ int, horas_semanales_ int)
 BEGIN
-	update gabd.curso
+	update gadb.curso
 	set anio=anio_,ciclo=ciclo_,nombre=nombre_,creditos=creditos_,horas_semanales=horas_semanales
 	where codigo=codigo_ and carrera_codigo=carrera_codigo_;
 END
@@ -49,7 +49,7 @@ DROP PROCEDURE IF EXISTS PRC_INS_CARRERA;
 DELIMITER //
 CREATE PROCEDURE PRC_INS_CARRERA(codigo_ varchar(10), titulo_ varchar(25),nombre_ varchar(100))
 BEGIN
-	insert into gabd.carrera (codigo, titulo, nombre) 
+	insert into gadb.carrera (codigo, titulo, nombre) 
 	values (codigo_,titulo_,nombre_);
 END
 //
@@ -60,7 +60,7 @@ DROP PROCEDURE IF EXISTS PRC_DEL_CARRERA;
 DELIMITER //
 CREATE PROCEDURE PRC_DEL_CARRERA(codigo_ varchar(10))
 BEGIN 
-	delete from gabd.carrera 
+	delete from gadb.carrera 
 	where codigo=codigo_;
 END
 //
@@ -71,7 +71,7 @@ DROP PROCEDURE IF EXISTS PRC_UPD_CARRERA;
 DELIMITER //
 CREATE PROCEDURE PRC_UPD_CARRERA(codigo_ varchar(10), titulo_ varchar(25),nombre_ varchar(100))
 BEGIN
-	update gabd.carrera
+	update gadb.carrera
 	set codigo=codigo_,titulo=titulo_,nombre=nombre_
 	where codigo=codigo_;
 END
