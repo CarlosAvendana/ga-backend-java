e73baa39690f1cb61f43c853cf7a12cd1425850e
LuisFelipeP98 <61294912+LuisFelipeP98@users.noreply.github.com>
2020-02-29 16:15:38 -0600

    exceptions

--- gaBackend/src/gaBackend/modelo/dao/GestorCurso.java
+++ gaBackend/src/gaBackend/modelo/dao/GestorCurso.java
@@ -1,16 +1,24 @@
 package gaBackend.modelo.dao;
 
+import exceptions.GlobalException;
+import exceptions.NoDataException;
+import gaBackend.modelo.Curso;
+import static gaBackend.modelo.dao.Service.connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author carlos
  */
 public class GestorCurso {
     
-    private static final String BUSCARCURSO="";
-    private static final String INSERTARCURSO="";
-    private static final String BORRARCURSO="";
-    private static final String ACTUALIZACURSO="";
-    private static final String LISTARCURSOS="";
+    
+    private static final String INSERTARCURSO="{call PRC_INS_CURSO(?,?,?)}";
+    private static final String BORRARCURSO="{call PRC_DEL_CURSO(?,?,?)}";
+    private static final String ACTUALIZACURSO="{call PRC_UPD_CURSO(?,?,?)}";
+    private static final String LISTARCURSOS="{?=call PRC_ObtieneTODOS_CURSO()}";
     
     private static GestorCurso instancia = null;
 
@@ -21,4 +29,16 @@ public class GestorCurso {
         return instancia;
     }
 
+    public List<Curso> listarCursos() throws NoDataException, GlobalException{
+        List<Curso> cursos= new ArrayList<>();
+                try {
+            connection();
+        } catch (ClassNotFoundException ex) {
+            throw new GlobalException("No se ha localizado el Driver");
+        } catch (SQLException e) {
+            throw new NoDataException("La base de datos no se encuentra disponible");
+        }
+        
+        return cursos;
+    }
 }
