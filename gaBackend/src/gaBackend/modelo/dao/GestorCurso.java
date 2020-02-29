f20516e94b210f39225286bcba82edd2b5720e52
LuisFelipeP98 <61294912+LuisFelipeP98@users.noreply.github.com>
2020-02-29 15:54:30 -0600

    CURSOS

--- gaBackend/src/gaBackend/modelo/dao/GestorCurso.java
+++ gaBackend/src/gaBackend/modelo/dao/GestorCurso.java
@@ -5,7 +5,13 @@ package gaBackend.modelo.dao;
  * @author carlos
  */
 public class GestorCurso {
-
+    
+    private static final String BUSCARCURSO="";
+    private static final String INSERTARCURSO="";
+    private static final String BORRARCURSO="";
+    private static final String ACTUALIZACURSO="";
+    private static final String LISTARCURSOS="";
+    
     private static GestorCurso instancia = null;
 
     public static GestorCurso obtenerInstancia() {
