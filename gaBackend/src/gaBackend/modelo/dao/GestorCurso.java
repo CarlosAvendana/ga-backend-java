e593513353d33784b26c9275749715cddc2ce645
LuisFelipeP98 <61294912+LuisFelipeP98@users.noreply.github.com>
2020-02-29 22:18:51 -0600

    GestorCurso completo

--- gaBackend/src/gaBackend/modelo/dao/GestorCurso.java
+++ gaBackend/src/gaBackend/modelo/dao/GestorCurso.java
@@ -5,6 +5,8 @@ import exceptions.NoDataException;
 import gaBackend.modelo.Curso;
 import static gaBackend.modelo.dao.Service.connection;
 import static gaBackend.modelo.dao.Service.disconnect;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -30,14 +32,96 @@ public class GestorCurso {
         }
         return instancia;
     }
+
+    public void insertarCurso(Curso curso) throws GlobalException, NoDataException {
+        try {
+            connection();
+        } catch (ClassNotFoundException e) {
+            throw new GlobalException("No se ha localizado el driver");
+        } catch (SQLException e) {
+            throw new NoDataException("La base de datos no se encuentra disponible");
+        }
+        CallableStatement pstmt = null;
+        try {
+            pstmt = connection.prepareCall(INSERTARCURSO);
+            pstmt.setString(1, curso.getCodigo());
+            pstmt.setString(2, curso.getCarrera_codigo());
+            pstmt.setString(3, curso.getAnio());
+            pstmt.setString(4, curso.getCiclo());
+            pstmt.setString(5, curso.getNombre());
+            pstmt.setInt(6, curso.getCreditos());
+            pstmt.setInt(7, curso.getHoras_semanales());
+
+            boolean resultado = pstmt.execute();
+            if (resultado == true) {
+            }
+        } catch (SQLException e) {
+
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                disconnect();
+            } catch (SQLException e) {
+
+            }
+        }
+
+    }
+
+    public void actualizarCurso(Curso curso) throws NoDataException, GlobalException, SQLException {
+        try {
+            connection();
+            PreparedStatement stm = connection.prepareStatement(ACTUALIZACURSO);
+
+            stm.clearParameters();
+
+            stm.setString(1, curso.getCodigo());
+            stm.setString(2, curso.getCarrera_codigo());
+            stm.setString(3, curso.getAnio());
+            stm.setString(4, curso.getCiclo());
+            stm.setString(5, curso.getNombre());
+            stm.setInt(6, curso.getCreditos());
+            stm.setInt(7, curso.getHoras_semanales());
+            if (stm.executeUpdate() != 1) {
+                throw new SQLException();
+            }
+            disconnect();
+        } catch (ClassNotFoundException ex) {
+            throw new GlobalException("No se ha localizado el Driver");
+        } catch (SQLException e) {
+            throw new NoDataException("La base de datos no se encuentra disponible");
+        }
+    }
     
-    public List<Curso> listarCursos() throws GlobalException, NoDataException  {
+    public void eliminarCurso(String codigo) throws GlobalException, NoDataException{
+    try {
+            connection();
+            PreparedStatement stm = connection.prepareStatement(BORRARCURSO);
+
+            stm.clearParameters();
+
+            stm.setString(1, codigo);
+
+            if (stm.executeUpdate() != 1) {
+                throw new SQLException();
+            }
+            disconnect();
+        } catch (ClassNotFoundException ex) {
+            throw new GlobalException("No se ha localizado el Driver");
+        } catch (SQLException e) {
+            throw new NoDataException("La base de datos no se encuentra disponible");
+        }
+    }
+
+    public List<Curso> listarCursos() throws GlobalException, NoDataException {
         List<Curso> cursos = new ArrayList<>();
         try {
             connection();
             Statement stm = connection.createStatement();
             ResultSet rs = stm.executeQuery(LISTARCURSOS);
-            while(rs.next()){
+            while (rs.next()) {
                 String codigoB = rs.getString("codigo");
                 String carrera_codigoB = rs.getString("carrera_codigo");
                 String anioB = rs.getString("anio");
@@ -45,15 +129,15 @@ public class GestorCurso {
                 String nombreB = rs.getString("nombre");
                 int creditosB = rs.getInt("creditos");
                 int horasB = rs.getInt("horas_semanales");
-                
-                cursos.add(new Curso(codigoB,carrera_codigoB,anioB,cicloB,nombreB,creditosB,horasB));
-            } 
+
+                cursos.add(new Curso(codigoB, carrera_codigoB, anioB, cicloB, nombreB, creditosB, horasB));
+            }
             disconnect();
         } catch (ClassNotFoundException ex) {
             throw new GlobalException("No se ha localizado el Driver");
         } catch (SQLException e) {
             throw new NoDataException("La base de datos no se encuentra disponible");
-        }     
+        }
         return cursos;
     }
 }
